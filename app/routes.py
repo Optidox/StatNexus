deb6aeb9e13657744569624b244051cb28a3d177
optidox <optidox@gmail.com>
2021-11-19 14:03:04 -0500

    initial flask commit

---
+++ app/routes.py
@@ -0,0 +1,11 @@
+from app import app
+from flask import render_template
+
+@app.route('/')
+@app.route('/index')
+def index():
+    return render_template('index.html')
+
+@app.route('/profile')
+def profile():
+    return "Hello, User!"
