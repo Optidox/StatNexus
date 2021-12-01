1f770b36862e76e33b8dc9d6ab477039f7a13576
Opti <optidox@gmail.com>
2021-11-30 21:56:03 -0500

    finish routing and templating

--- app/routes.py
+++ app/routes.py
@@ -7,12 +7,22 @@ from flask import render_template
 def index():
     return render_template('index.html')
 
-
 @app.route('/profile')
 def profile():
     return "Hello, User!"
 
+@app.route('/contact')
+def contact():
+    return "contact"
 
-@app.route('/ph')
-def ph():
-    return "ph"
+@app.route('/league')
+def league():
+    return "league"
+
+@app.route('/destiny')
+def destiny():
+    return "leagues"
+
+@app.route('/osu')
+def osu():
+    return "osu"
