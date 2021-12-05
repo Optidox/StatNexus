ae07f30febfc024139923059bd62348b8a80bbe0
optidox <optidox@gmail.com>
2021-12-05 17:40:50 -0500

    authentication debugged

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,5 @@
 from app import app, db
-from flask import render_template, flash, redirect, url_for
+from flask import render_template, flash, redirect, url_for, request
 from app.forms import LoginForm, RegistrationForm
 from flask_login import current_user, login_user, logout_user
 from app.models import User
@@ -39,18 +39,23 @@ def osu():
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+    print(login)
     if current_user.is_authenticated:
         return redirect(url_for('index'))
     form = LoginForm()
     if form.validate_on_submit():
+        print('validated')
         user = User.query.filter_by(username=form.username.data).first()
-        if user is not None or not user.check_password(form.password.data):
+        print(user)
+        print(user.check_password(form.password.data))
+        if user is None or not user.check_password(form.password.data):
             flash('Invalid Username or Password')
             return redirect(url_for('login'))
         login_user(user, remember=form.remember_me.data)
         next_page = request.args.get('next')
         if not next_page or url_parse(next_page).netloc != '':
             next_page = url_for('index')
+        print(next_page)
         return redirect(next_page)
     return render_template('temp_login.html', form=form)
 
