2dc13a36cf37d2148c53b782249b0ae210af8b0e
Optidox <optidox@gmail.com>
2021-12-13 08:31:46 +0000

    Database and gitignore maybe work finally?!

--- app/models.py
+++ app/models.py
@@ -6,9 +6,9 @@ from flask_login import UserMixin
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     alt_id = db.Column(db.Unicode(256), unique=True, nullable=False)
-    email = db.Column(db.String(320), unique=True, nullable=False)
-    username = db.Column(db.String(64), unique=True, nullable=False)
-    password_hash = db.Column(db.String(256), nullable=False)
+    email = db.Column(db.Unicode(320), unique=True, nullable=False)
+    username = db.Column(db.Unicode(64), unique=True, nullable=False)
+    password_hash = db.Column(db.Unicode(256), nullable=False)
     League = db.relationship('League', backref='league_id', lazy=True)
     Bungie = db.relationship('Bungie', backref='destiny_id', lazy=True)
     Osu = db.relationship('Osu', backref='osu_id', lazy=True)
@@ -33,18 +33,18 @@ def load_user(uid):
 
 class League(db.Model):
     id = db.Column(db.Integer, db.ForeignKey('user.id'), primary_key=True)
-    username = db.Column(db.String(16))
+    username = db.Column(db.Unicode(16))
 
 
 class Bungie(db.Model):
     id = db.Column(db.Integer, db.ForeignKey('user.id'), primary_key=True)
-    access_token = db.Column(db.String(2048))
-    refresh_token = db.Column(db.String(1024))
+    access_token = db.Column(db.UnicodeText())
+    refresh_token = db.Column(db.UnicodeText())
     expiration_time = db.Column(db.Integer)
 
 
 class Osu(db.Model):
     id = db.Column(db.Integer, db.ForeignKey('user.id'), primary_key=True)
-    access_token = db.Column(db.String(2048))
-    refresh_token = db.Column(db.String(1024))
+    access_token = db.Column(db.UnicodeText())
+    refresh_token = db.Column(db.UnicodeText())
     expiration_time = db.Column(db.Integer)
