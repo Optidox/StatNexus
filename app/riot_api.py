5da90734b1fbe26ed26f5c60d2a1693892eb1db0
Optidox <optidox@gmail.com>
2021-12-14 06:29:12 +0000

    league git add --all

--- app/riot_api.py
+++ app/riot_api.py
@@ -16,10 +16,10 @@ def _get_summoner_id(name):
     return response.json()['id']
 
 def _get_champ_name_from_id(id):
-    with open('static/json/champion.json') as json_file:
+    with open("app/static/json/champion.json") as json_file:
         champs = json.load(json_file)
-        for champ, info in champs['data']:
-            if info['key'] == id:
+        for champ, info in champs['data'].items():
+            if info['key'] == str(id):
                 return info['name']
 
 def get_league_stats(name):
@@ -29,7 +29,6 @@ def get_league_stats(name):
     ranked_stats = response.json()
     response = requests.get('https://na1.api.riotgames.com/lol/champion-mastery/v4/champion-masteries/by-summoner/' + id, headers=_api_header())
     champ_stats = response.json()
-    print(json.dumps(champ_stats, indent=4))
     user_stats = {
         'Summoner Name' : ranked_stats[0]['summonerName'],
         'Most Played Champions' : '{0}, {1}, {2}'.format(_get_champ_name_from_id(champ_stats[0]['championId']),
@@ -41,7 +40,7 @@ def get_league_stats(name):
     for mode in ranked_stats:
         name = 'Solo/Duo ' if 'SOLO' in mode['queueType'] else 'Flex '
         user_stats[name + 'rank'] = '{0} {1} {2} LP'.format(mode['tier'], mode['rank'], mode['leaguePoints'])
-        user_stats[name + 'wins/losses'] = '{0}W {1}L'.format(mode['wins'], mode[losses])
-        user_stats[name + 'win %'] = '{}%'.format(round((mode['wins'] / mode['losses']) * 100, 1))
+        user_stats[name + 'wins/losses'] = '{0}W {1}L'.format(mode['wins'], mode['losses'])
+        user_stats[name + 'win %'] = '{}%'.format(round((mode['wins'] / (mode['losses'] + mode['wins']) * 100), 1))
 
     return user_stats
